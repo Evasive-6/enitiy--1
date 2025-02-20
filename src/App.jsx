d43b0e1b811e47b00a63a6831d47a98d514bedde
Evasive-6 <albinshiju285@gmail.com>
2025-02-20 11:14:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 // src/App.jsx
 
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router, Route, and Routes
 import Home from './Home';
+import AddBook from './components/AddBook'; // Import the AddBook component
 import './App.css'; // Import global styles
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-book" element={<AddBook />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
